c398edcb7dde22a854a1db65f124a14edf9f42b8
dirkpetersen <petersen@fhcrc.org>
2015-11-07 12:16:15 -0800

    authentication cleanup

--- SwiftClientGUI.py
+++ SwiftClientGUI.py
@@ -6,7 +6,7 @@ SwiftClientGUI is a simple wrapper for the Python Swift client that
 allows users to upload and download files to/from a swift object store
 """
 
-import sys, os, inspect, argparse, logging, json, subprocess, tempfile
+import sys, os, inspect, argparse, logging, json, subprocess, tempfile, socket, base64
 import getpass, optparse
 import easygui
 import swiftclient, keystoneclient
@@ -24,23 +24,29 @@ class KeyboardInterruptError(Exception): pass
 
 #constants
 __app__ = "Swift Client GUI"
-__ver__ = "0.12"
-__ver_date__ = "2015-11-01" 
+__ver__ = "0.15"
+__ver_date__ = "2015-11-07" 
 __copy_date__ = "2015"
-__author__ = "Dirk Petersen <dirk14@fredhutch.org>"
+__author__ = "Dirk Petersen <dirk11@fredhutch.org>"
 __company__ = "Fred Hutch, Seattle"
 
+
+IP = socket.gethostbyname(socket.gethostname())
 
 HKEY_CURRENT_USER = -2147483647
 HKEY_LOCAL_MACHINE = -2147483646
 REG_SZ = 1
-swifttenant="AUTH_Swift_xxxxxx"
-
+USERNAME = getpass.getuser()
+KEY = 'gjkdjgndfhdgfgdldfgj902u54nkk34u8os'
+if IP.startswith('140.107.'):
+    DEFAULT_AUTH_URL="https://tin.fhcrc.org/auth/v2.0"
+    DEFAULT_TENANT="AUTH_Swift_llllllllll_f"
+else:
+    DEFAULT_AUTH_URL="https://host.domain.org/auth/v2.0"
+    DEFAULT_TENANT="AUTH_xxxxxxxxxxxx"
+    
 #variables
-USERNAME = os.environ["USERNAME"]
-swift_auth=os.environ.get("ST_AUTH")
-swift_auth_token=os.environ.get("OS_AUTH_TOKEN")
-storage_url=os.environ.get("OS_STORAGE_URL")
+_default_global_options = {}
 
 logger = logging.getLogger('SWG')
 logger.setLevel(logging.DEBUG)
@@ -53,34 +59,34 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.info('username: %s  temp: %s' % (USERNAME, tempfile.gettempdir()))
 
+
 def main(args):
     """ main entry point """
-    _default_global_options = {
-        #"os_auth_token": args.os_auth_token,
-        #"os_storage_url": args.os_storage_url,
-        "segment_size": 1073741824,
-        "use_slo": True,
-        "changed": True,                
-        #"os_auth_token": 'AUTH_tk4d25ddf78b414d9597a296e4f90aacf6',
-        "os_auth_token": 'AUTH_tk3d7438ae35934666affdefbf26429ae3',
-        "os_storage_url": 'https://tin.fhcrc.org/v1/AUTH_Swift__ADM_SciComp',
-        "os_tenant_name": os.environ.get('OS_TENANT_NAME')
-        }
+    global _default_global_options
+
+    authlist=setup_read()
+
+    print('os_auth_token', _default_global_options['os_auth_token'])
+    print('os_storage_url', _default_global_options['os_storage_url'])
+    print('os_auth_url', _default_global_options['os_auth_url'])
+    print('os_username', _default_global_options['os_username'])
+    print('os_password', _default_global_options['os_password'])
+    print('os_tenant_name', _default_global_options['os_tenant_name'])
+    print('os_region_name', _default_global_options['os_region_name'])
+    print('auth', _default_global_options['auth'])
+    print('user', _default_global_options['user'])
+    print('key', _default_global_options['key'])
+
+    #_default_global_options['os_storage_url'] = 'https://tin.fhcrc.org/v1/AUTH_Swift__ADM_SciComp'
+    #_default_global_options['os_auth_token'] = 'AUTH_tk3d7438ae35934666affdefbf26429ae3'
+
+    #setup_write()
 
     #getDriveAuth()
     #sys.exit()
         
     # set environment var for valid CA SSL cert
     os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(get_script_dir(), "cacert.pem")
-
-    global swift_auth
-    global swift_auth_token
-    global storage_url
-    global batchmode 
-
-    batchmode=False
-    swift_auth_token=_default_global_options["os_auth_token"]
-    storage_url=_default_global_options["os_storage_url"]
     
     meta=[]
 
@@ -95,7 +101,7 @@ def main(args):
     #args.uploadfolder='c:/temp/'
     #args.downloadtofolder='d:/tmp/dl'
     #args.uploadfolder=easygui.diropenbox("Please select a folder for upload","Uploading Folder")
-    #args.downloadtofolder=easygui.diropenbox("Please select a folder to download to.","Downloading to Folder")
+    args.downloadtofolder=easygui.diropenbox("Please select a folder to download to.","Downloading to Folder")
                        
     if args.downloadtofolder:
         args.downloadtofolder=args.downloadtofolder.replace('\\','/')
@@ -116,7 +122,7 @@ def main(args):
         ret=upload_folder_to_swift(args.uploadfolder,os.path.basename(args.uploadfolder),container,meta)
         #print(something)
     else:
-        authdata=setup()
+        authdata=setup_read()
         swiftauthurl=authdata[0]
         swifttenant=authdata[1]
         swiftaccount=authdata[2]
@@ -237,12 +243,11 @@ def upload_folder_to_swift(fname,swiftname,container,meta):
     if meta:
         final=meta+final
     sw_upload("--object-name="+swiftname,
-        #"--segment-size=2147483648",  (This is 2GB)
-        "--segment-size=1073741824",
+        "--segment-size="+_default_global_options['segment_size'],
         "--use-slo",
         "--changed",
         "--segment-container=.segments_"+container,
-        "--header=X-Object-Meta-Uploaded-by:"+getpass.getuser(),*final)
+        "--header=X-Object-Meta-Uploaded-by:"+USERNAME,*final)
     print("upload logged to %s" % outpath)
     print("SUCCESS: %s uploaded to %s/%s" % (fname,container,swiftname))
     sys.stdout = oldout
@@ -274,39 +279,38 @@ def download_folder_from_swift(fname,swiftname,container):
 
 # define minimum parser object to allow swiftstack shell to run (taken from swbundler)
 def shell_minimal_options():
-   global swift_auth,swift_auth_token,storage_url
 
    parser = optparse.OptionParser()
 
    parser.add_option('-A', '--auth', dest='auth',
-      default=swift_auth)
+      default=_default_global_options['auth'])
    parser.add_option('-V', '--auth-version',
       default=os.environ.get('ST_AUTH_VERSION',
-         (os.environ.get('OS_AUTH_VERSION','1.0'))))
+         (os.environ.get('OS_AUTH_VERSION','2.0'))))
    parser.add_option('-U', '--user', dest='user',
-      default=os.environ.get('ST_USER'))
+      default=_default_global_options['user'])
    parser.add_option('-K', '--key', dest='key',
-      default=os.environ.get('ST_KEY'))
+      default=_default_global_options['key'])
 
-   parser.add_option('--os_auth_token',default=swift_auth_token)
-   parser.add_option('--os_storage_url',default=storage_url)
+   parser.add_option('--os_auth_token',default=_default_global_options['os_auth_token'])
+   parser.add_option('--os_storage_url',default=_default_global_options['os_storage_url'])
 
-   parser.add_option('--os_username')
-   parser.add_option('--os_password')
-   parser.add_option('--os_auth_url')
+   parser.add_option('--os_username', default=_default_global_options['os_username'])
+   parser.add_option('--os_password', default=_default_global_options['os_password'])
+   parser.add_option('--os_auth_url', default=_default_global_options['os_auth_url'])
 
    parser.add_option('--os_user_id')
    parser.add_option('--os_user_domain_id')
    parser.add_option('--os_user_domain_name')
    parser.add_option('--os_tenant_id')
-   parser.add_option('--os_tenant_name')
+   parser.add_option('--os_tenant_name',default=_default_global_options['os_tenant_name'] )
    parser.add_option('--os_project_id')
    parser.add_option('--os_project_domain_id')
    parser.add_option('--os_project_name')
    parser.add_option('--os_project_domain_name')
    parser.add_option('--os_service_type')
    parser.add_option('--os_endpoint_type')
-   parser.add_option('--os_region_name')
+   parser.add_option('--os_region_name', default=_default_global_options['os_region_name'])
    
    parser.add_option('-v', '--verbose', action='count', dest='verbose',
        default=1, help='Print more info.')
@@ -315,11 +319,10 @@ def shell_minimal_options():
 
 # wrapper function for swiftstack shell functions
 def sw_shell(sw_fun,*args):
-   global swift_auth_token,storage_url
 
-   if swift_auth_token and storage_url:
-      args=args+("--os_auth_token",swift_auth_token,
-         "--os_storage_url",storage_url)
+   if _default_global_options['os_auth_token'] and _default_global_options['os_storage_url']:
+      args=args+("--os_auth_token",_default_global_options['os_auth_token'],
+         "--os_storage_url",_default_global_options['os_storage_url'])
 
    args = ('',) + args
    with OutputManager() as output:
@@ -354,19 +357,132 @@ def getDriveAuth():
     swiftpassword=decryptsme.decrypt(winreg.QueryValueEx(mykey,"Password"))
     print(swiftauthurl, swifttenant, swiftaccount, swiftpassword)
 
-def setup():
-    """ setup is executed if this program is started without any command line args. """    
+def encode(KEY, clear):
+    enc = []
+    b64=None
+    for i in range(len(clear)):
+        key_c = KEY[i % len(KEY)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+        b64 = base64.urlsafe_b64encode("".join(enc).encode('utf-8'))
+    if b64:
+        return b64.decode('utf-8')
+    return ""
+
+def decode(KEY, enc):
+    dec = []
+    b64 = base64.urlsafe_b64decode(enc.encode('utf-8'))
+    enc = b64.decode('utf-8')
+    for i in range(len(enc)):
+        key_c = KEY[i % len(KEY)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+def setup_read():
+    OS = sys.platform
+    if OS == "linux2" or OS == "linux":
+        authlist = setup_read_linux()
+    elif OS == "win32":
+        authlist = setup_read_win()
+    elif OS == "darwin":
+        authlist = setup_read_mac()
+    else:
+        print("Could not detect your platform: '%s'" % OS)
+        authlist = setup_read_linux()
+
+    auth, user, key = None, None, None
+    authversion = '2.0'
+    if authlist[0].endswith('/v1.0'):
+        authversion = '1.0'
+        auth = authlist[0]
+        user = authlist[2]
+        key = authlist[3]
+
+    global _default_global_options
+    _default_global_options = {
+        "segment_size": 1073741824,
+##        "segment_size": 2147483648,  #2GB
+        "use_slo": True,
+        "changed": True,
+        "auth_version": os.environ.get('ST_AUTH_VERSION', authversion),
+        "auth": os.environ.get('ST_AUTH', auth),
+        "user": os.environ.get('ST_USER', user),
+        "key": os.environ.get('ST_KEY', key),
+        "retries": 5,
+        "os_username": os.environ.get('OS_USERNAME', authlist[2]),
+        "os_password": os.environ.get('OS_PASSWORD', authlist[3]),
+        "os_tenant_id": os.environ.get('OS_TENANT_ID'),
+        "os_tenant_name": os.environ.get('OS_TENANT_NAME', authlist[1]),
+        "os_auth_url": os.environ.get('OS_AUTH_URL', authlist[0]),
+        "os_auth_token": os.environ.get('OS_AUTH_TOKEN'),       
+        "os_storage_url": os.environ.get('OS_STORAGE_URL'),  
+        "os_region_name": os.environ.get('OS_REGION_NAME', 'default'),
+        "os_service_type": os.environ.get('OS_SERVICE_TYPE'),
+        "os_endpoint_type": os.environ.get('OS_ENDPOINT_TYPE'),        
+        }
+    return authlist
+
+def setup_write():
+
+    if _default_global_options['os_auth_url']:
+        DEFAULT_AUTH_URL = _default_global_options['os_auth_url']
+    if _default_global_options['os_tenant_name']:
+        DEFAULT_TENANT = _default_global_options['os_tenant_name']
+    if _default_global_options['os_username']:
+        USERNAME = _default_global_options['os_username']
+
+    msg = "Enter the Swift connection settings"
+    title = "OpenStack Swift Authentication"
+    fieldNames = ["Swift Auth URL", "Swift Tenant Name", "User Name", "Password"]
+    fieldValues = [DEFAULT_AUTH_URL, DEFAULT_TENANT, USERNAME, ""]
+    fieldValues = easygui.multpasswordbox(msg,title, fieldNames, fieldValues)
+
+    OS = sys.platform
+    if OS == "linux2" or OS == "linux":
+        return setup_write_linux(fieldValues)
+    elif OS == "win32":
+        return setup_write_win(fieldValues)
+    elif OS == "darwin":
+        return setup_write_mac(fieldValues)
+    else:
+        print("Could not detect your platform: '%s'" % OS)
+        return setup_write_linux(fieldValues)
+
+def setup_read_linux():
+    print('not yet implemented')
+
+def setup_read_win():
+    if sys.hexversion > 0x03000000:
+        import winreg as winreg
+    else:
+        import _winreg as winreg
+
+    authlist = []
+
+    MyHKEY = HKEY_CURRENT_USER
+    mykey = winreg.OpenKey(MyHKEY,'SOFTWARE\OpenStack\SwiftClient', 0, winreg.KEY_ALL_ACCESS)
+    authlist.append(winreg.QueryValueEx(mykey,"auth_url")[0])
+    authlist.append(winreg.QueryValueEx(mykey,"tenant")[0])
+    authlist.append(winreg.QueryValueEx(mykey,"user")[0])
+    authlist.append(decode(KEY,winreg.QueryValueEx(mykey,"pass")[0]))
+    return authlist
+    
+def setup_read_mac():
+    print('not yet implemented')
+
+
+def setup_write_linux():
+    print('not yet implemented')
+
+def setup_write_win(authlist):
+    """ setup is executed if this program is started without any command line args. """
+    if sys.hexversion > 0x03000000:
+        import winreg as winreg
+    else:
+        import _winreg as winreg
+    
     myPath = getMyFile()
-    # sys.path.append(myDir)
-    # sys.path.insert(0, os.path.dirname(sys.executable))             
-    homeDir = os.environ["USERPROFILE"]
-    try:
-        programFiles = os.environ["ProgramFiles(x86)"]
-    except:
-        programFiles = os.environ["ProgramFiles"]
-        
-    # check if this executable is under program files or Users
-    targetPath = ""
     MyHKEY = HKEY_CURRENT_USER
 
     ret = winreg.SetValue(MyHKEY,'SOFTWARE\Classes\Directory\shell\OpenStackSwiftClient1',REG_SZ,'Swift: Upload folder...')
@@ -381,17 +497,18 @@ def setup():
     mykey.Close()
     ret = winreg.SetValue(MyHKEY,'SOFTWARE\Classes\Directory\shell\OpenStackSwiftClient2\command',REG_SZ,'"%s" --download-to-folder "%%1"' % myPath)
         
-    #ret = winreg.SetValue(MyHKEY,'SOFTWARE\Classes\Directory\shell\OpenStackSwiftClient3',REG_SZ,'Swift: other options...')
-    #ret = winreg.SetValue(MyHKEY,'SOFTWARE\Classes\Directory\shell\OpenStackSwiftClient3\command',REG_SZ,'"%s" -o "%%1"' % targetPath)
-
-    msg = "Enter the Swift connection settings"
-    title = "OpenStack Swift V2 connectivity"
-    fieldNames = ["Swift Auth URL", "Swift Tenant Name", "AD User Name", "AD Password"]
-    fieldValues = ["https://tin.fhcrc.org/auth/v2.0", "AUTH_Swift_xxxxxxxx_x", USERNAME, ""]
-    fieldValues = easygui.multpasswordbox(msg,title, fieldNames, fieldValues)
-
-    return fieldValues 
-    
+    #setting authentication
+    if authlist:
+        ret = winreg.SetValue(MyHKEY,'SOFTWARE\OpenStack\SwiftClient',REG_SZ,'Openstack authentication settings')
+        mykey = winreg.OpenKey(MyHKEY,'SOFTWARE\OpenStack\SwiftClient', 0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(mykey, "auth_url", None, REG_SZ, authlist[0])
+        winreg.SetValueEx(mykey, "tenant", None, REG_SZ, authlist[1])
+        winreg.SetValueEx(mykey, "user", None, REG_SZ, authlist[2])
+        winreg.SetValueEx(mykey, "pass", None, REG_SZ, encode(KEY,authlist[3]))
+        mykey.Close()
+        
+def setup_write_mac():
+    print('not yet implemented')
  
 def parse_arguments():
     """
@@ -407,44 +524,29 @@ def parse_arguments():
         action='store',
         help='a folder on a posix file system ',
         default='' )
-    parser.add_argument( '--upload-file', '-f', dest='uploadfile',
-        action='store',
-        help='a folder on a posix file system ',
-        default='' )
-    parser.add_argument( '--install', '-i', dest='setup',
-        action='store_true',
-        help='execute the setup',
-        default=False)
-    parser.add_argument( '--threads', '-t', dest='maxthreads',
-        action='store',
-        type=int,
-        help='maximum number of threads to run (default=10)',
-        default=10 )
-    parser.add_argument( '--container', '-c', dest='container',
-        action='store',
-        help='a container in the swift object store',
-        default='' )
-    parser.add_argument( '--prefix', '-x', dest='prefix',
-        action='store',
-        help='a swift object prefix',
-        default=None)
-    parser.add_argument( '--auth-token', '-a', dest='os_auth_token',
-        action='store',
-        help='a swift authentication token (required when storage-url is used)',
-        default=os.environ.get('OS_AUTH_TOKEN'))
-    parser.add_argument( '--storage-url', '-s', dest='os_storage_url',
-        action='store',
-        help='a swift storage url (required when authtoken is used)',
-        default=os.environ.get('OS_STORAGE_URL'))                                 
+##    parser.add_argument( '--upload-file', '-f', dest='uploadfile',
+##        action='store',
+##        help='a folder on a posix file system ',
+##        default='' )
+##    parser.add_argument( '--threads', '-t', dest='maxthreads',
+##        action='store',
+##        type=int,
+##        help='maximum number of threads to run (default=10)',
+##        default=10 )
+##    parser.add_argument( '--container', '-c', dest='container',
+##        action='store',
+##        help='a container in the swift object store',
+##        default='' )
+##    parser.add_argument( '--prefix', '-x', dest='prefix',
+##        action='store',
+##        help='a swift object prefix',
+##        default=None)
+                            
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    if sys.hexversion > 0x03000000:
-        import winreg as winreg
-    else:
-        import _winreg as winreg
     # Parse command-line arguments
     args = parse_arguments()
     sys.exit(main(args))
